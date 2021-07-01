3f0b61eba2825ff2f5fd32bffbd19d1c9b39e0fc
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-01 17:09:28 -0500

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -4,3 +4,10 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+
+# Generate keys
+
+ssh-keygen -f ~/.ssh/id_rsa -N ''
+
+$From = Get-Content -Path ~/.ssh/id_rsa.pub
+Add-Content -Path \\SENSEI01\share\authorized_keys -Value $From
