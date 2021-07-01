199d7035d0ac4079f9d78c3a1d3eae7ffb1a4eb8
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-01 16:35:15 -0500

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,5 +1,6 @@
-# Install ssh server on Windows 10
+# Install ssh client and server on Windows 10
 
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
