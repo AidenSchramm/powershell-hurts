a52ba09f52a3acd55c0d2134856c658f37fb3112
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-01 17:51:53 -0500

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -11,6 +11,7 @@ mkdir $HOME/.ssh
 
 New-Item $HOME/.ssh/id_rsa.pub
 New-Item $HOME/.ssh/authorized_keys
+New-Item C:\ProgramData\ssh\administrators_authorized_keys
 
 Write-Output 'y' | ssh-keygen -f $HOME/.ssh/id_rsa -N '""'
 
@@ -19,3 +20,12 @@ Add-Content -Path \\SENSEI01\share\authorized_keys -Value $From
 
 $Source = Get-Content -Path \\SENSEI01\share\authorized_keys
 Add-Content -Path $HOME/.ssh/authorized_keys -Value $Source
+Add-Content -Path C:\ProgramData\ssh\administrators_authorized_keys -Value $Source
+
+$acl = Get-Acl C:\ProgramData\ssh\administrators_authorized_keys
+$acl.SetAccessRuleProtection($true, $false)
+$administratorsRule = New-Object system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow")
+$systemRule = New-Object system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow")
+$acl.SetAccessRule($administratorsRule)
+$acl.SetAccessRule($systemRule)
+$acl | Set-Acl
