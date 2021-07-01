ba1fa463fd112905c9735ae0c8e0e506bc8127cc
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-01 17:46:01 -0500

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -10,7 +10,12 @@ Set-Service -Name sshd -StartupType 'Automatic'
 mkdir $HOME/.ssh
 
 New-Item $HOME/.ssh/id_rsa.pub
+New-Item $HOME/.ssh/authorized_keys
+
 Write-Output 'y' | ssh-keygen -f $HOME/.ssh/id_rsa -N '""'
 
 $From = Get-Content -Path $HOME/.ssh/id_rsa.pub
 Add-Content -Path \\SENSEI01\share\authorized_keys -Value $From
+
+$Source = Get-Content -Path \\SENSEI01\share\authorized_keys
+Add-Content -Path $HOME/.ssh/authorized_keys -Value $Source
