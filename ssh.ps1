08e6a720595ffd9857869e9b0e6b0d49729d9038
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-13 13:57:44 -0500

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,5 +1,15 @@
 # Install ssh client and server on Windows 10
 
+# Elevate privledges
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
