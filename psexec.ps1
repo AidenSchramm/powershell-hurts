6bd2694b1df8b5a9e318ea16619d077aef34e5ce
Aiden Schramm <47539574+AidenSchramm@users.noreply.github.com>
2021-07-13 13:52:27 -0500

    Update psexec.ps1

--- psexec.ps1
+++ psexec.ps1
@@ -1,4 +1,7 @@
-﻿if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+# Windows is dumb and adds something weird to the beginning of this file
+
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
   Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
